0d678ce9bd38bfbf6f222dca9894784338e31fc0
AsthaBista <aasthabista@hotmail.com>
2020-03-31 10:42:56 +0200

    Second commit

--- CaseStudy/CourseProject2.R
+++ CaseStudy/CourseProject2.R
@@ -1,1 +1,17 @@
-ne
+library(dplyr)
+
+## Preparing the data
+zippedDat<-unzip("exdata_data_NEI_data.zip")
+pmData<-readRDS("summarySCC_PM25.rds")
+head(pmData)
+sourceClass<-readRDS("Source_Classification_Code.rds")
+head(sourceClass)
+
+## Q.N.1
+## Have total emissions from PM2.5 decreased in the United States 
+## from 1999 to 2008? Using the base plotting system, make a plot
+## showing the total PM2.5 emission from all sources for each of the 
+## years 1999, 2002, 2005, and 2008.
+pmGp<-pmData %>% group_by(year) %>%
+  summarize(Total = sum(Emissions,na.rm = TRUE))
+pmGp
