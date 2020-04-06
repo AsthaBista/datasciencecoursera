8290d36e364ae13380dda6f44a1b93cd3bf95186
AsthaBista <37456314+AsthaBista@users.noreply.github.com>
2020-04-06 19:12:37 +0200

    Delete Project_W2.R

--- ReproducibleResearch/Project_W2.R
+++
@@ -1,8 +0,0 @@
-## Download the data into a temporary file and read the data
-url<-"https://d396qusza40orc.cloudfront.net/repdata%2Fdata%2Factivity.zip"
-z<-tempfile()
-download.file(url,z)
-file<- unzip(z)
-data <- read.csv(file)
-unlink(z)
-head(data)
